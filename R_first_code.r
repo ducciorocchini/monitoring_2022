fd79b7cfc4d44343757dda1128553e31063980b7
ducciorocchini <ducciorocchini@gmail.com>
2021-10-22 10:55:07 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -17,6 +17,45 @@ plot(water, fishes)
 # a table in R is called data frame
 
 streams <- data.frame(water, fishes)
+streams
+
+
+# From now on, we are going to import and/or export data!
+
+# For Linux (Ubuntu, Fedora, Debian, Mandriva) users
+setwd("~/lab/")
+
+# setwd for Windows
+# setwd("C:/lab/")
+
+# setwd Mac
+# setwd("/Users/yourname/lab/")
+
+# Let's export our table!
+# https://www.rdocumentation.org/packages/utils/versions/3.6.2/topics/write.table
+write.table(streams, file="my_first_table.txt")
+
+# Some colleagues did send us a table How to import it in R?
+read.table("my_first_table.txt")
+# let's assign it to an object uinside R
+ducciotable <- read.table("my_first_table.txt")
+
+# the first statistics for lazy beautiful people
+summary(ducciotable)
+
+# Marta does not like water
+# Marta wants to get info only on fishes
+summary(ducciotable$fishes)
+
+# histogram
+hist(ducciotable$fishes)
+hist(ducciotable$water)
+
+
+
+
+
+
 
 
 
