efee6eaf78a349656d7d70a6e916b2711a44696e
ducciorocchini <ducciorocchini@gmail.com>
2021-10-25 11:03:49 +0200

    Create R_code_remote_sensing.r

---
+++ R_code_remote_sensing.r
@@ -0,0 +1,8 @@
+# R code for ecosystem monitoring by remote sensing
+# First of all, we need to install additional packages
+# raster package to manage image data
+# https://cran.r-project.org/web/packages/raster/index.html
+
+install.packages("raster")
+
+library(raster)
