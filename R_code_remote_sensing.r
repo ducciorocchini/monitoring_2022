63c02819f063dbe43a41dbc48cb7c961fb504ac3
ducciorocchini <ducciorocchini@gmail.com>
2021-10-29 11:01:42 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -3,6 +3,35 @@
 # raster package to manage image data
 # https://cran.r-project.org/web/packages/raster/index.html
 
-install.packages("raster")
+# install.packages("raster")
 
 library(raster)
+
+setwd("~/lab/") # Linux
+# setwd("C:/lab/")  # windows
+# setwd("/Users/name/lab/") # mac
+
+# We are going to import satellite data
+# objects cannot be numbers
+l2011 <- brick("p224r63_2011.grd")
+
+l2011
+
+# plot(l2011)
+
+# B1 is the reflectance in the blue band
+# B2 is the reflectance in the green band
+# B3 is the reflectance in the red band
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011, col=cl)
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+
+
+
+
+
+
+
