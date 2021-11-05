7953aabcda254425a43709fa02cac6f1685e13c6
ducciorocchini <ducciorocchini@gmail.com>
2021-11-05 11:06:14 +0100

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -7,6 +7,7 @@
 
 library(raster)
 
+# Set the working directory
 setwd("~/lab/") # Linux
 # setwd("C:/lab/")  # windows
 # setwd("/Users/name/lab/") # mac
@@ -27,6 +28,62 @@ cl <- colorRampPalette(c("black","grey","light grey"))(100)
 plot(l2011, col=cl)
 
 plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
+
+#----------------- day 2
+
+# B1 is the reflectance in the blue band
+# B2 is the reflectance in the green band
+# B3 is the reflectance in the red band
+# B4 is the reflectance in the NIR band
+
+# let's plot the green band
+plot(l2011$B2_sre)
+
+cl <- colorRampPalette(c("black","grey","light grey"))(100)
+plot(l2011$B2_sre, col=cl)
+
+# change the colorRampPalette with dark green, green, and light green, e.g. clg 
+clg <- colorRampPalette(c("dark green","green","light green"))(100)
+plot(l2011$B2_sre, col=clg)
+
+# do the same for the blue band using "dark blue", "blue", and "light blue"
+# B1
+clb <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(l2011$B1_sre, col=clb)
+
+# plot both images in just one multiframe graph
+par(mfrow=c(1,2)) # the first number is the number of rows in the multiframe, while the second one is the mnumber of columns
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+# plot both images in just one multiframe graph with two rows and just one column
+par(mfrow=c(2,1))
+plot(l2011$B1_sre, col=clb)
+plot(l2011$B2_sre, col=clg)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
