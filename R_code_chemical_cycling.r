b0bf2cbaefc4b8f3b93158cbcd8a6601c13278c2
ducciorocchini <ducciorocchini@gmail.com>
2021-11-26 10:51:54 +0100

    Update R_code_chemical_cycling.r

--- R_code_chemical_cycling.r
+++ R_code_chemical_cycling.r
@@ -1,5 +1,7 @@
 # R code for chemical cycling study
 # time series of NO2 change in Europe during the lockdown
+# https://www.esa.int/Applications/Observing_the_Earth/Copernicus/Sentinel-5P/Coronavirus_lockdown_leading_to_drop_in_pollution_across_Europe
+# https://acp.copernicus.org/preprints/acp-2020-995/acp-2020-995.pdf
 
 library(raster)
 
@@ -21,9 +23,58 @@ en13 <- raster("EN_0013.png")
 plot(en13, col=cl)
 
 # Build a multiframe window with 2 rows and 1 column with the par function
+par(mfrow=c(2,1))
+plot(en01, col=cl)
+plot(en13, col=cl)
 
+# import all the images
+EN01 <- raster("EN_0001.png")
+EN02 <- raster("EN_0002.png")
+EN03 <- raster("EN_0003.png")
+EN04 <- raster("EN_0004.png")
+EN05 <- raster("EN_0005.png")
+EN06 <- raster("EN_0006.png")
+EN07 <- raster("EN_0007.png")
+EN08 <- raster("EN_0008.png")
+EN09 <- raster("EN_0009.png")
+EN10 <- raster("EN_0010.png")
+EN11 <- raster("EN_0011.png")
+EN12 <- raster("EN_0012.png")
+EN13 <- raster("EN_0013.png")
 
+# plot all the data together
+par(mfrow=c(4,4))
+plot(EN01, col=cl)
+plot(EN02, col=cl)
+plot(EN03, col=cl)
+plot(EN04, col=cl)
+plot(EN05, col=cl)
+plot(EN06, col=cl)
+plot(EN07, col=cl)
+plot(EN08, col=cl)
+plot(EN09, col=cl)
+plot(EN10, col=cl)
+plot(EN11, col=cl)
+plot(EN12, col=cl)
+plot(EN13, col=cl)
 
+EN <- stack(EN01, EN02, EN03, EN04, EN05, EN06, EN07, EN08, EN09, EN10, EN11, EN12, EN13)
 
+# plot the stack altogether
+plot(EN, col=cl)
 
+# plot only the first image from the stack
+# check the stack names
+EN
+plot(EN$EN_0001, col=cl)
+
+# rgb
+plotRGB(EN, r=1, g=7, b=13, stretch="lin")
+
+# difference
+
+# pairs
+pairs(EN)
+
+# direct import
 
