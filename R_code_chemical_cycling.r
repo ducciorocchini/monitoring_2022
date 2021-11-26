11f366d8a3ce5694fbe07481ced3449d2eba6beb
ducciorocchini <ducciorocchini@gmail.com>
2021-11-26 09:45:23 +0100

    Create R_code_chemical_cycling.r

---
+++ R_code_chemical_cycling.r
@@ -0,0 +1,12 @@
+# R code for chemical cycling study
+# time series of NO2 change in Europe during the lockdown
+
+
+# Set the working directory
+setwd("~/lab/en/") # Linux 
+# setwd("C:/lab/en/")  # windows
+# setwd("/Users/name/lab/en/") # mac
+
+
+
+
