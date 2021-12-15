931e82af901b827c4a396efbc1891f7da0ea4340
ducciorocchini <ducciorocchini@gmail.com>
2021-12-15 18:10:03 +0100

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,19 @@
+# R code for uploading and visualizing Coprnicus data in R
+
+# install.packages("ncdf4")
+library(ncdf4)
+library(raster)
+
+# Set the working directory
+setwd("~/lab/copernicus/") # Linux 
+# setwd("C:/lab//copernicus/")  # windows
+# setwd("/Users/name/lab/copernicus/") # mac
+
+snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+
+snow20211214
+
+
+
+
+
