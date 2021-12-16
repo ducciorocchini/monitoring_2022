f1ade49decaedad3551c70ede00c6b9bf7ef00e6
ducciorocchini <ducciorocchini@gmail.com>
2021-12-16 11:24:44 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -13,6 +13,14 @@ snow20211214 <- raster("c_gls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
 
 snow20211214
 
+plot(snow20211214)
+
+cl <- colorRampPalette(c('dark blue','blue','light'))(100) # 
+plot(snow20211214, col=cl)
+
+
+
+
 
 
 
