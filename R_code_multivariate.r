cf32cc0172b98e40beb8e072715085f1e2d6e881
ducciorocchini <ducciorocchini@gmail.com>
2021-12-22 12:43:34 +0100

    Update R_code_multivariate.r

--- R_code_multivariate.r
+++ R_code_multivariate.r
@@ -1,6 +1,29 @@
-# R code for measuring community interactions!
+# R_code_multivar.r
 
-# install.package("vegan")
+# install.packages("vegan")
 library(vegan)
 
+# Linux
+setwd("~/lab/")
 
+load("biomes_multivar.RData")
+ls()
+
+# plot per species matrix
+biomes
+
+multivar <- decorana(biomes)
+multivar
+
+plot(multivar)
+
+# biomes names in the graph:
+attach(biomes_types)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
+
+pdf("multivar.pdf")
+plot(multivar)
+ordiellipse(multivar, type, col=c("black","red","green","blue"), kind = "ehull", lwd=3)
+ordispider(multivar, type, col=c("black","red","green","blue"), label = T) 
+dev.off()
